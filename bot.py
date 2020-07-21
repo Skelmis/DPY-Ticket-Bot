d3b88aaf245bcae588808700756619d66cb1a2b6
Skelmis <47520067+Skelmis@users.noreply.github.com>
2020-07-22 03:11:02 +1200

    Heres the bot!

---
+++ bot.py
@@ -0,0 +1,169 @@
+import traceback
+
+import discord
+from discord.ext import commands
+
+from utils.util import (
+    CreateNewTicket,
+    SudoCreateNewTicket,
+    CloseTicket,
+    GetTicketSetupMessageId,
+    CheckIfTicket,
+    ReactionCreateNewTicket,
+    SetupNewTicketMessage,
+    CheckIfValidReactionMessage,
+    ReactionCloseTicket,
+)
+from utils.json import read_json
+
+bot = commands.Bot(
+    command_prefix="..", case_insensitive=True, owner_id=271612318947868673
+)
+secret_file = read_json("secrets")
+
+bot.new_ticket_channel_id = None
+bot.log_channel_id = None
+bot.category_id = None
+bot.staff_role_id = None
+
+bot.colors = {
+    "WHITE": 0xFFFFFF,
+    "AQUA": 0x1ABC9C,
+    "GREEN": 0x2ECC71,
+    "BLUE": 0x3498DB,
+    "PURPLE": 0x9B59B6,
+    "LUMINOUS_VIVID_PINK": 0xE91E63,
+    "GOLD": 0xF1C40F,
+    "ORANGE": 0xE67E22,
+    "RED": 0xE74C3C,
+    "NAVY": 0x34495E,
+    "DARK_AQUA": 0x11806A,
+    "DARK_GREEN": 0x1F8B4C,
+    "DARK_BLUE": 0x206694,
+    "DARK_PURPLE": 0x71368A,
+    "DARK_VIVID_PINK": 0xAD1457,
+    "DARK_GOLD": 0xC27C0E,
+    "DARK_ORANGE": 0xA84300,
+    "DARK_RED": 0x992D22,
+    "DARK_NAVY": 0x2C3E50,
+}
+bot.colorList = [c for c in bot.colors.values()]
+
+
+@bot.event
+async def on_ready():
+    print("Lesh go!")
+    await bot.change_presence(activity=discord.Game(name=".new for a ticket"))
+
+
+@bot.event
+async def on_raw_reaction_add(payload):
+    # Check if its the bot adding the reaction
+    if payload.user_id == bot.user.id:
+        return
+
+    # Check if its a valid reaction
+    reaction = str(payload.emoji)
+    if reaction not in ["🔒", "✅"]:
+        return
+
+    # Check its a valid reaction channel
+    if not payload.channel_id == bot.new_ticket_channel_id and not CheckIfTicket(
+        str(payload.channel_id)
+    ):
+        return
+
+    # Check its a valid message
+    if not CheckIfValidReactionMessage(payload.message_id):
+        return
+
+    # Soooo, its valid message and reaction so go do logic bois
+
+    data = read_json("config")
+    if payload.message_id == data["ticketSetupMessageId"] and reaction == "✅":
+        # We want a new ticket...
+        await ReactionCreateNewTicket(bot, payload)
+        return
+
+    elif reaction == "🔒":
+        # Simply add a tick to the message
+        channel = bot.get_channel(payload.channel_id)
+        message = await channel.fetch_message(payload.message_id)
+        await message.add_reaction("✅")
+
+    elif reaction == "✅":
+        # Time to delete the ticket!
+        guild = bot.get_guild(payload.guild_id)
+        member = await guild.fetch_member(payload.user_id)
+
+        channel = bot.get_channel(payload.channel_id)
+        await ReactionCloseTicket(bot, channel, member)
+
+
+@bot.event
+async def on_raw_reaction_remove(payload):
+    # Check if its the bot adding the reaction
+    if payload.user_id == bot.user.id:
+        return
+
+    # Check if its a valid reaction
+    reaction = str(payload.emoji)
+    if reaction not in ["🔒"]:
+        return
+
+    # Check its a valid reaction channel
+    if not payload.channel_id == bot.new_ticket_channel_id and not CheckIfTicket(
+        str(payload.channel_id)
+    ):
+        return
+
+    # Check its a valid message
+    if not CheckIfValidReactionMessage(payload.message_id):
+        return
+
+    if reaction == "🔒":
+        # Simply remove a tick from the message
+        guild = bot.get_guild(payload.guild_id)
+        member = await guild.fetch_member(bot.user.id)
+
+        channel = bot.get_channel(payload.channel_id)
+        message = await channel.fetch_message(payload.message_id)
+        await message.remove_reaction("✅", member)
+
+
+@bot.command()
+async def new(ctx, *, subject=None):
+    await CreateNewTicket(bot, ctx, subject)
+
+
+@bot.command()
+async def close(ctx, *, reason=None):
+    await CloseTicket(bot, ctx, reason)
+
+
+@bot.command()
+@commands.is_owner()
+async def sudonew(ctx, user: discord.Member):
+    await ctx.message.delete()
+    await SudoCreateNewTicket(bot, ctx.guild, user, ctx.message)
+
+
+@bot.command()
+@commands.is_owner()
+async def setup(ctx):
+    await SetupNewTicketMessage(bot)
+
+
+@bot.command()
+@commands.is_owner()
+async def echo(ctx, channel: discord.TextChannel, *, content):
+    await ctx.message.delete()
+    embed = discord.Embed(
+        description=content, color=0x808080, timestamp=ctx.message.created_at
+    )
+    embed.set_author(name=ctx.guild.me.display_name, icon_url=ctx.guild.me.avatar_url)
+    await channel.send(embed=embed)
+
+
+if __name__ == "__main__":
+    bot.run(secret_file["token"])
---
+++ utils/util.py
@@ -0,0 +1,344 @@
+import random
+import discord
+
+from utils.json import read_json, write_json
+
+
+def GetTicketCount():
+    data = read_json("config")
+    return data["ticketCount"]
+
+
+def IncrementTicketCount():
+    data = read_json("config")
+    data["ticketCount"] += 1
+    write_json(data, "config")
+
+
+def GetTicketSetupMessageId():
+    data = read_json("config")
+    return data["ticketSetupMessageId"]
+
+
+async def SendLog(
+    bot,
+    ctx,
+    channel,
+    contentOne="Default Message",
+    contentTwo="\uFEFF",
+    color=0x808080,
+    file=None,
+):
+    embed = discord.Embed(
+        title=f"{contentOne}",
+        description=f"{contentTwo}",
+        colour=color,
+        timestamp=ctx.message.created_at,
+    )
+    embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
+    await channel.send(embed=embed)
+    if file:
+        await channel.send(file=file)
+
+
+async def ReactionSendLog(
+    bot,
+    author,
+    channel,
+    contentOne="Default Message",
+    contentTwo="\uFEFF",
+    color=0x808080,
+    file=None,
+):
+    embed = discord.Embed(
+        title=f"{contentOne}", description=f"{contentTwo}", colour=color,
+    )
+    embed.set_author(name=author.name, icon_url=author.avatar_url)
+    await channel.send(embed=embed)
+    if file:
+        await channel.send(file=file)
+
+
+async def SudoSendLog(
+    bot,
+    author,
+    message,
+    channel,
+    contentOne="Default Message",
+    contentTwo="\uFEFF",
+    color=0x808080,
+    file=None,
+):
+    embed = discord.Embed(
+        title=f"{contentOne}",
+        description=f"{contentTwo}",
+        colour=color,
+        timestamp=message.created_at,
+    )
+    embed.set_author(name=author.name, icon_url=author.avatar_url)
+    await channel.send(embed=embed)
+    if file:
+        await channel.send(file=file)
+
+
+def LogNewTicketChannel(channelId, ticketId):
+    data = read_json("config")
+    data[str(channelId)] = {}
+    data[str(channelId)]["id"] = ticketId
+    data[str(channelId)]["reactionMsgId"] = None
+    write_json(data, "config")
+
+
+def CheckIfTicket(channelId):
+    data = read_json("config")
+    return str(channelId) in data
+
+
+def GetTicketId(channelId):
+    data = read_json("config")
+    return data[str(channelId)]["id"]
+
+
+def RemoveTicket(channelId):
+    data = read_json("config")
+    data.pop(str(channelId))
+    write_json(data, "config")
+
+
+async def NewTicketSubjectSender(ctx, channel, subject):
+    if subject == "No subject specified.":
+        return
+    embed = discord.Embed(
+        title="Provided subject for ticket:",
+        description=subject,
+        color=0x808080,
+        timestamp=ctx.message.created_at,
+    )
+    embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
+    await channel.send(embed=embed)
+
+
+async def NewTicketEmbedSender(bot, author, channel):
+    content = f"""
+    **Hello {author.display_name}**
+
+    This is your support ticket, how can we help you?
+
+    `Our team will be with you shortly.`
+    """
+    embed = discord.Embed(description=content, color=0x808080)
+    m = await channel.send(f"{author.mention} | <@&{bot.staff_role_id}>", embed=embed)
+    await m.add_reaction("🔒")
+
+    data = read_json("config")
+    data[str(channel.id)]["reactionMsgId"] = m.id
+    write_json(data, "config")
+
+
+async def ReactionCreateNewTicket(bot, payload):
+    subject = "No subject specified."
+    ticketId = GetTicketCount() + 1
+
+    guild = bot.get_guild(int(payload.guild_id))
+    author = guild.get_member(int(payload.user_id))
+    staffRole = guild.get_role(bot.staff_role_id)
+    logChannel = bot.get_channel(bot.log_channel_id)
+
+    overwrites = {
+        guild.default_role: discord.PermissionOverwrite(read_messages=False),
+        guild.me: discord.PermissionOverwrite(read_messages=True),
+        staffRole: discord.PermissionOverwrite(read_messages=True),
+        author: discord.PermissionOverwrite(read_messages=True),
+    }
+
+    channel = await guild.create_text_channel(
+        name=f"Support Ticket #{ticketId}",
+        overwrites=overwrites,
+        category=bot.get_channel(bot.category_id),
+    )
+
+    LogNewTicketChannel(channel.id, ticketId)
+    await ReactionSendLog(
+        bot,
+        author,
+        logChannel,
+        f"Created ticket with ID {ticketId}",
+        f"Ticket Creator: {author.mention}(`{author.id}`)\nChannel: {channel.mention}({channel.name})\nSubject: {subject}",
+        0xB4DA55,
+    )
+
+    await NewTicketEmbedSender(bot, author, channel)
+    IncrementTicketCount()
+
+
+async def CreateNewTicket(bot, ctx, subject=None):
+    subject = subject or "No subject specified."
+    ticketId = GetTicketCount() + 1
+    staffRole = ctx.guild.get_role(bot.staff_role_id)
+    logChannel = bot.get_channel(bot.log_channel_id)
+
+    overwrites = {
+        ctx.guild.default_role: discord.PermissionOverwrite(read_messages=False),
+        ctx.guild.me: discord.PermissionOverwrite(read_messages=True),
+        staffRole: discord.PermissionOverwrite(read_messages=True),
+        ctx.author: discord.PermissionOverwrite(read_messages=True),
+    }
+
+    channel = await ctx.guild.create_text_channel(
+        name=f"Support Ticket #{ticketId}",
+        overwrites=overwrites,
+        category=bot.get_channel(bot.category_id),
+    )
+
+    LogNewTicketChannel(channel.id, ticketId)
+    await SendLog(
+        bot,
+        ctx,
+        logChannel,
+        f"Created ticket with ID {ticketId}",
+        f"Ticket Creator: {ctx.author.mention}(`{ctx.author.id}`)\nChannel: {channel.mention}({channel.name})\nSubject: {subject}",
+        0xB4DA55,
+    )
+
+    await NewTicketEmbedSender(bot, ctx.message.author, channel)
+    await NewTicketSubjectSender(ctx, channel, subject)
+    await ctx.message.delete()
+    IncrementTicketCount()
+
+
+async def SudoCreateNewTicket(bot, guild, author, message):
+    ticketId = GetTicketCount() + 1
+    staffRole = guild.get_role(bot.staff_role_id)
+    logChannel = bot.get_channel(bot.log_channel_id)
+
+    overwrites = {
+        guild.default_role: discord.PermissionOverwrite(read_messages=False),
+        guild.me: discord.PermissionOverwrite(read_messages=True),
+        staffRole: discord.PermissionOverwrite(read_messages=True),
+        author: discord.PermissionOverwrite(read_messages=True),
+    }
+
+    channel = await guild.create_text_channel(
+        name=f"Support Ticket #{ticketId}",
+        overwrites=overwrites,
+        category=bot.get_channel(bot.category_id),
+    )
+
+    LogNewTicketChannel(channel.id, ticketId)
+    await SudoSendLog(
+        bot,
+        author,
+        message,
+        logChannel,
+        f"Created ticket with ID {ticketId}",
+        f"Ticket Creator: {author.mention}(`{author.id}`)\nChannel: {channel.mention}({channel.name})",
+        0xB4DA55,
+    )
+    await NewTicketEmbedSender(bot, author, channel)
+    IncrementTicketCount()
+
+
+async def CloseTicket(bot, ctx, reason=None):
+    if not CheckIfTicket(ctx.channel.id):
+        await ctx.send("I cannot close this as it is not a ticket.")
+        await ctx.message.delete()
+        return
+
+    # If u get here it means its a valid ticket
+    reason = reason or "No closing reason specified."
+    ticketId = GetTicketId(ctx.channel.id)
+    messages = await ctx.channel.history(limit=None, oldest_first=True).flatten()
+    ticketContent = " ".join(
+        [f"{message.content} | {message.author.name}\n" for message in messages]
+    )
+    with open(f"tickets/{ticketId}.txt", "w", encoding="utf8") as f:
+        f.write(f"Here is the message log for ticket ID {ticketId}\n----------\n\n")
+        f.write(ticketContent)
+
+    fileObject = discord.File(f"tickets/{ticketId}.txt")
+    logChannel = bot.get_channel(bot.log_channel_id)
+    await SendLog(
+        bot,
+        ctx,
+        logChannel,
+        f"Closed Ticked ID {ticketId}",
+        f"Close Reason: {reason}",
+        0xF42069,
+        fileObject,
+    )
+    await ctx.channel.delete()
+
+
+async def ReactionCloseTicket(bot, channel, author):
+    if not CheckIfTicket(channel.id):
+        await channel.send("I cannot close this as it is not a ticket.")
+        return
+
+    # If u get here it means its a valid ticket
+    reason = "No closing reason specified."
+    ticketId = GetTicketId(channel.id)
+    messages = await channel.history(limit=None, oldest_first=True).flatten()
+    ticketContent = " ".join(
+        [f"{message.content} | {message.author.name}\n" for message in messages]
+    )
+    with open(f"tickets/{ticketId}.txt", "w", encoding="utf8") as f:
+        f.write(f"Here is the message log for ticket ID {ticketId}\n----------\n\n")
+        f.write(ticketContent)
+
+    fileObject = discord.File(f"tickets/{ticketId}.txt")
+    logChannel = bot.get_channel(bot.log_channel_id)
+    await ReactionSendLog(
+        bot,
+        author,
+        logChannel,
+        f"Closed Ticked ID {ticketId}",
+        f"Close Reason: {reason}",
+        0xF42069,
+        fileObject,
+    )
+
+    try:
+        file = discord.File(f"tickets/{ticketId}.txt")
+        embed = discord.Embed(description="Here is your support ticket log file")
+        await author.send(embed=embed)
+        await author.send(file=file)
+    except:
+        await ReactionSendLog(
+            bot,
+            author,
+            logChannel,
+            contentTwo=f"Failed to dm {author.name} for this ticket",
+        )
+
+    await channel.delete()
+
+
+def CheckIfValidReactionMessage(msgId):
+    data = read_json("config")
+
+    if data["ticketSetupMessageId"] == msgId:
+        return True
+
+    data.pop("ticketSetupMessageId")
+    data.pop("ticketCount")
+    for value in data.values():
+        if value["reactionMsgId"] == msgId:
+            return True
+
+    return False
+
+
+async def SetupNewTicketMessage(bot):
+    data = read_json("config")
+    channel = bot.get_channel(bot.new_ticket_channel_id)
+
+    embed = discord.Embed(
+        title="Hand Trained Services",
+        description="To purchase a service or enquire about one you must react with a tick",
+        color=0xB4DA55,
+    )
+    m = await channel.send(embed=embed)
+    await m.add_reaction("✅")
+    data["ticketSetupMessageId"] = m.id
+
+    write_json(data, "config")
