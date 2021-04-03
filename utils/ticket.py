e7df802ab0384f9966292470c39559fff3eb3d0b
skelmis <skelmis.craft@gmail.com>
2021-04-03 22:23:34 +1300

    :sparkles: Begin wrok on new bot

--- utils/ticket.py
+++ utils/ticket.py
@@ -1,10 +1,17 @@
-import discord
+from typing import Union
 
+from custom_context import MyContext
 from utils.json_loader import read_json, write_json
 
 
 class Ticket:
-    def __init__(self, message: discord.Message):
+    def __init__(self, ctx: MyContext):
+        self.ctx = ctx
+
+    def is_ticket(self):
+        return Ticket.check_is_ticket(self.ctx.message.channel.id)
+
+    def save_new_ticket(self):
         pass
 
     @staticmethod
@@ -12,7 +19,7 @@ class Ticket:
         return read_json("config")
 
     @staticmethod
-    def is_ticket(channel_id):
+    def check_is_ticket(channel_id: Union[str, int]):
         return str(channel_id) in Ticket.get_config()
 
     @staticmethod
@@ -27,9 +34,12 @@ class Ticket:
         write_json(data, "config")
 
     @staticmethod
-    def build(message: discord.Message):
-        if not Ticket.is_ticket(message.channel.id):
-            return None
+    def get_setup_message_id():
+        data = Ticket.get_config()
+        return data["ticket_setup_message_id"]
 
-        ticket = Ticket(message)
-        return ticket
+    @staticmethod
+    def remove_ticket(channel_id: Union[str, int]):
+        data = Ticket.get_config()
+        data.pop(str(channel_id))
+        write_json(data, "config")
